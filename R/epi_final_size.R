7d59819aab81e66fa5c03fd30a4f180760c6ad31
adamkucharski <adam.kucharski@lshtm.ac.uk>
2017-03-29 11:18:04 +0100

    add relative susceptibility

--- R/epi_final_size.R
+++ R/epi_final_size.R
@@ -4,6 +4,7 @@
 #' @param r0  Basic reproduction number. Default is 2
 #' @param contact_matrix  Social contact matrix. Entry mm_ij gives average number of contacts in group i reported by participants in group j
 #' @param demography_vector  Demography vector. Entry pp_i gives proportion of total population in group i (model will normalise if needed)
+#' @param prop_suscep  Proportion of each group susceptible. Null assumption is fully susceptible
 #' @keywords epidemic model
 #' @export
 #' @examples
@@ -11,14 +12,16 @@
 
 # DEFINE FINAL SIZE MODEL
 
-epi_final_size<-function(r0=2,contact_matrix,demography_vector){
+epi_final_size<-function(r0=2,contact_matrix,demography_vector,prop_suscep){
   
   # Check inputs
-  if(length(contact_matrix[1,])!=length(demography_vector)){stop("demography vector be same size as contact matrix")}
+  if(is.null(prop_suscep)){prop_suscep=demography_vector*0+1} # Assume fully susceptible if no entry
+  if(length(contact_matrix[1,])!=length(demography_vector)){stop("demography vector needs to be same size as contact matrix")}
+  if(length(demography_vector)!=length(prop_suscep)){stop("demography vector needs to be same size as susceptibility vector")}
   pp0=as.numeric(demography_vector/sum(demography_vector))
   
   # Scale next generation matrix so max eigenvalue=r0
-  mm0=as.matrix(contact_matrix)
+  mm0=as.matrix(contact_matrix) * prop_suscep
   mm0=r0*mm0/max(Re(eigen(mm0)$values))
   
   # Define transmission matrix A = mm_ij*pp_j/pp_i
